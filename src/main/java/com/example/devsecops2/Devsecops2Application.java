919ecf8df8e7f607a5487d1477064177727272ef
Johan Gourmand <johan.gourmand@gmail.com>
2025-03-18 17:33:15 +0100

    ddd

---
+++ src/main/java/com/example/devsecops2/Devsecops2Application.java
@@ -0,0 +1,16 @@
+package com.example.devsecops2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devsecops2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devsecops2Application.class, args);
+	}
+
+	public static class ServiceWeb {
+
+	}
+}
