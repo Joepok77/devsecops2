5536f740183bfcb59bd764a10daebcdc09b4a0ef
Johan Gourmand <johan.gourmand@gmail.com>
2025-03-25 16:14:30 +0100

    lanca 500

--- src/main/java/com/example/devsecops2/ServiceWeb.java
+++ src/main/java/com/example/devsecops2/ServiceWeb.java
@@ -18,7 +18,7 @@ public class ServiceWeb {
         cars.add(car);
         car = new Car("CC33DD", "peugeot", 500);
         cars.add(car);
-        car = new Car("BB44EE", "lancia", 800);
+        car = new Car("BB44EE", "lancia", 500);
         cars.add(car);
     }
 
